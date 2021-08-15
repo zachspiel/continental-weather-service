abba2b102a4734b152c57bb736deca68ba939c51
zachspiel <zachspielberger@gmail.com>
2021-08-15 00:00:53 -0700

    Updated files

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const app = express();
 app.use(cors());
