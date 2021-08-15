ac58e82d1f5a87c3a56a9fd55ec5ab14d8b2d3f3
zachspiel <zachspielberger@gmail.com>
2021-08-15 09:30:25 -0700

    Update filepaths to client build

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'));
 });
 
 const contactEmail = nodemailer.createTransport({
